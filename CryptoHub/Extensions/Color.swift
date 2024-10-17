57a32a9326ae8d60e034e067bac12c119ea41f35
IacobZanoci <114911332+IacobZanoci@users.noreply.github.com>
2024-10-17 14:56:43 +0300

    First config

---
+++ CryptoHub/Extensions/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  CryptoHub
+//
+//  Created by Iacob Zanoci on 17/10/2024.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GrColor")
+    let red = Color("RdColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
