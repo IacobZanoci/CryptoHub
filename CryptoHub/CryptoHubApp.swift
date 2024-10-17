483ad4f6725086faca5a623cf573d5a68a989119
IacobZanoci <114911332+IacobZanoci@users.noreply.github.com>
2024-10-17 12:45:11 +0300

    Initial Commit

---
+++ CryptoHub/CryptoHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoHubApp.swift
+//  CryptoHub
+//
+//  Created by Iacob Zanoci on 17/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
