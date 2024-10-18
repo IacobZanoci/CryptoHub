96fa893a567c26ef33b871e245e901c8919b0153
IacobZanoci <114911332+IacobZanoci@users.noreply.github.com>
2024-10-18 22:24:31 +0300

    [feat] Dynamic List to display downloaded coins

---
+++ CryptoHub/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewModel.swift
+//  CryptoHub
+//
+//  Created by Iacob Zanoci on 18/10/2024.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+    @Published var allCoins: [Coin] = []
+    @Published var portfolioCoins: [Coin] = []
+    
+    init () {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        }
+    }
+    
+}
